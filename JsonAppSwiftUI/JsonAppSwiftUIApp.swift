8c6bc1a36783d2596bbca0de098fa9014db9855f
Jose Valentin Abundo Hernandez <valentin.abundo@bambu-mobile.com>
2025-07-20 16:41:37 -0600

    Initial Commit

---
+++ JsonAppSwiftUI/JsonAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JsonAppSwiftUIApp.swift
+//  JsonAppSwiftUI
+//
+//  Created by Developer on 20/07/25.
+//
+
+import SwiftUI
+
+@main
+struct JsonAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
